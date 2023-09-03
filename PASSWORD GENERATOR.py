6223385a5b6e167f545620f3f97c14e1ab7c4342
NAKKA.VENKATA RAGHURAM <113761707+20a31a04t3@users.noreply.github.com>
2023-09-03 22:54:11 +0530

    PASSWORD GENERATOR.py

--- PASSWORD GENERATOR.py
+++ PASSWORD GENERATOR.py
@@ -1,11 +1,11 @@
 import string
 import random
-len=int(input("enter the length of the password:"))
-lower=string.ascii_lowercase
-upper=string.ascii_uppercase
+length=int(input("enter the length of the password:"))
+lower1=string.ascii_lowercase
+upper1=string.ascii_uppercase
 digit=string.digits
 symbols=string.punctuation
-str=lower+upper+digit+symbols
-pwd=random.sample(str,len)
+str=lower1+upper1+digit+symbols
+pwd=random.sample(str,length)
 password=" ".join(pwd)
 print(password)
